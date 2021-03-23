2c10eca121ace6e60eb69e6e9b0e0b29184f08cd
ahmeda14960 <ahmedah@stanford.edu>
2021-03-23 04:49:11 +0000

    changed reqs

--- coinrun/coinrun/setup_utils.py
+++ coinrun/coinrun/setup_utils.py
@@ -8,6 +8,7 @@ def load_for_setup_if_necessary():
     restore_file(Config.RESTORE_ID)
 
 def restore_file(restore_id, load_key='default'):
+    print('Restoring config')
     if restore_id is not None:
         load_file = Config.get_load_filename(restore_id=restore_id)
         filepath = file_to_path(load_file)
@@ -26,9 +27,9 @@ def restore_file(restore_id, load_key='default'):
                 print('warning key %s not restored' % key)
 
         Config.parse_args_dict(sub_dict)
-    
-    # from coinrun.coinrunenv import init_args_and_threads
-    # init_args_and_threads(4)
+    print('Setting up threads')
+    from coinrun.coinrunenv import init_args_and_threads
+    init_args_and_threads(4)
 
 def setup_and_load(use_cmd_line_args=True, **kwargs):
     """
@@ -37,8 +38,9 @@ def setup_and_load(use_cmd_line_args=True, **kwargs):
     `use_cmd_line_args`: set to False to ignore command line arguments passed to the program
     `**kwargs`: override the defaults from `config.py` with these values
     """
+    print('Init config')
     args = Config.initialize_args(use_cmd_line_args=use_cmd_line_args, **kwargs)
-
+    print('Load setup')
     load_for_setup_if_necessary()
 
     return args
