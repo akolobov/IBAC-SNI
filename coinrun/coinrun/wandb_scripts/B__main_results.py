80908dcea26f472de9e0df8da0c998575abcfc00
ahmeda14960 <ahmedah@stanford.edu>
2021-04-25 18:03:54 +0000

    restructured wandb plotting scripts

---
+++ coinrun/coinrun/wandb_scripts/B__main_results.py
@@ -0,0 +1,212 @@
+import pandas as pd
+import os
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+from collections import defaultdict
+
+import time
+import glob
+
+from download_wandb_data import load_WandB_csvs
+
+AGENTS = ["ppo_goal_bogdan"]
+CLEAN_NAMES_AGENTS = ['Ours']
+
+selected_run_ids = {'ppo_goal_bogdan':'(.*smarterMYOW__5__3__.*)',
+                        'ppo':'.*',
+                        'ppo_diayn':'.*',
+                        'ppg':'.*',
+                        'ppo_curl':'.*'}
+
+agent2label = dict(zip(AGENTS,CLEAN_NAMES_AGENTS))
+
+files = glob.glob('../wandb_data/*.csv')
+params_to_load = ['agent','n_skills','n_knn','cluster_t','temp','environment']
+
+df = load_WandB_csvs(files,params_to_load,selected_run_ids,AGENTS,agent2label)
+
+print('#######################')
+print('Groups:')
+print(df['group'].unique())
+print('#######################')
+
+metrics = ['eprew','eprew_eval','silhouette_score']
+
+X_LEFT = 0
+X_RIGHT = 8e6
+
+rp = [-1029.86559098,  2344.5778132 , -1033.38786418,  -487.3693808 ,
+         298.50245209,   167.25393272]
+gp = [  551.32444915, -1098.30287507,   320.71732031,   258.50778539,
+         193.11772901,    30.32958789]
+bp = [  222.95535971, -1693.48546233,  2455.80348727,  -726.44075478,
+         -69.61151887,    67.591787  ]
+
+def clamp(n):
+    return min(255, max(0, n))
+
+def gradient(x, rfactors, gfactors, bfactors):
+    '''
+    Return the r,g,b values along the predefined gradient for
+    x in the range [0.0, 1.0].
+    '''
+    n = len(rfactors)
+    r = clamp(int(sum(rfactors[i] * (x**(n-1-i)) for i in range(n))))
+    g = clamp(int(sum(gfactors[i] * (x**(n-1-i)) for i in range(n))))
+    b = clamp(int(sum(bfactors[i] * (x**(n-1-i)) for i in range(n))))
+    return r, g, b
+
+
+def set_run_attributes(sub_df):
+    T = float(sub_df['temp'].unique()[-1])
+    label = 'T=%.1f' %(T)
+    col = gradient(T/10, rp, gp, bp)
+    return label, col, '-'
+
+xlim = (0,25e6,5e6)
+
+table_eval = r"""
+\begin{table}[ht]
+\centering
+\caption{Average eval returns collected after 25M of training frames, $\pm$ one standard deviation.}
+\resizebox{\linewidth}{!}{%
+\begin{tabular}{l||l|ll}
+\toprule
+"""
+table_train = r"""
+\begin{table}[ht]
+\centering
+\caption{Average train returns collected after 25M of training frames, $\pm$ one standard deviation.}
+\resizebox{\linewidth}{!}{%
+\begin{tabular}{l||l|ll}
+\toprule
+"""
+table_eval += "Env & " + ' & '.join(CLEAN_NAMES_AGENTS) +'\\\\ \n'
+reported_scores_eval = np.zeros((16,len(CLEAN_NAMES_AGENTS)))
+
+table_train += "Env & " + ' & '.join(CLEAN_NAMES_AGENTS) +'\\\\ \n'
+reported_scores_train = np.zeros((16,len(CLEAN_NAMES_AGENTS)))
+
+metrics = ['eprew','eprew_eval']
+
+for metric in metrics:
+    games_list = sorted(df['environment'].unique())
+    n_games = len(games_list)    
+    nrows = int( np.sqrt(n_games) )
+    ncols = n_games // nrows 
+    print('Metric: %s, Games: %d, rows: %d, cols: %d' % (metric,n_games,nrows,ncols) )
+
+    fig, axes = plt.subplots(nrows=nrows, ncols=ncols,figsize=(12, 12), sharex=False, sharey=False)
+    
+    row_idx, col_idx = 0,0
+    handles_, labels_ = [], []
+    for e_idx,env_name in enumerate(games_list):
+        if metric == 'eprew_eval':
+            row_str_eval = env_name + ' '
+        if metric == 'eprew':
+            row_str_train = env_name + ' '
+
+        ax = axes[row_idx][col_idx]
+
+        col_idx += 1
+        if col_idx >= ncols:
+            col_idx = 0
+            row_idx += 1
+        min_y, max_y = float('inf'), float('-inf')
+        
+        game_df = df[df['environment']==env_name]
+        
+        for group_name,group_df in game_df.groupby('group'):
+
+            group_df[metric] = group_df[metric].ewm(30).mean()
+            smooth_df_mu = group_df.groupby('custom_step').apply(np.mean)
+            smooth_df_std = group_df.groupby('custom_step').apply(np.std)
+
+            HP_LABEL, col, linestyle = set_run_attributes(smooth_df_mu)
+            
+            x = smooth_df_mu.index.to_numpy()
+            mu = smooth_df_mu[metric].to_numpy()
+            std = smooth_df_std[metric].to_numpy()
+            ax.plot(x,mu,color=np.array(col)/255.,label=HP_LABEL,linestyle=linestyle,linewidth=2)
+            ax.fill_between(x, mu-std, mu+std, alpha=0.1, color=np.array(col)/255.)
+            max_y = max(max_y,(mu).max())
+            min_y = min(min_y,(mu).min())
+
+            start_idx = np.max(np.where(x<X_RIGHT)[0])
+            # start_idx = len(mu)-10
+            best_idx = mu[:start_idx].argmax()#+start_idx
+
+            if metric == 'eprew_eval':
+                row_str_eval += '& %.1f$\pm$%.1f ' % (mu[best_idx],std[best_idx])
+            if metric == 'eprew':
+                row_str_train += '& %.1f$\pm$%.1f ' % (mu[best_idx],std[best_idx])
+            # add best score to table of scores for normalized absolute score computation
+            if metric == 'eprew_eval':
+                reported_scores_eval[e_idx,a_idx] = mu[best_idx]
+            if metric == 'eprew':
+                reported_scores_train[e_idx,a_idx] = mu[best_idx]
+
+        major_ticks = np.arange(X_LEFT, X_RIGHT, 1e6)
+
+        ax.set_xticks(major_ticks)
+        
+        ticks_x = ticker.FuncFormatter(lambda x, pos: '{0:g}'.format(x/1e6))
+        ax.xaxis.set_major_formatter(ticks_x)
+
+        for tick in ax.xaxis.get_major_ticks():
+            tick.label.set_fontsize(15) 
+        for tick in ax.yaxis.get_major_ticks():
+            tick.label.set_fontsize(15) 
+
+        try:
+            ax.set_ylim(min_y-(max_y-min_y)*0.1,max_y+(max_y-min_y)*0.1)
+        except:
+            pass
+        ax.set_xlim(X_LEFT,X_RIGHT)
+        
+        ax.set_title(env_name)
+        ax.set_xlabel('Timesteps (M)')
+        ax.set_ylabel('Average metric')
+
+        handles, labels = ax.get_legend_handles_labels()
+
+        if metric == 'eprew_eval':
+            row_str_eval += '\\\\ \n'
+            table_eval += row_str_eval
+        if metric == 'eprew':
+            row_str_train += '\\\\ \n'
+            table_train += row_str_train
+
+
+    plt.tight_layout()
+    plt.legend(handles,labels, loc='lower left', bbox_to_anchor=(0.5,0.5),prop={'size': 15}) # new_handles, new_labels,
+    
+    plt.savefig(os.path.join('..','wandb_plots','B__main_results_'+metric+'.png'),dpi=200)
+
+normed_scores_eval = (reported_scores_eval/np.tile(reported_scores_eval[:,1].reshape(16,1),(1,len(CLEAN_NAMES_AGENTS)))) # standardized by PPO
+normed_scores_eval[normed_scores_eval == 0] = np.nan
+normed_scores_eval = np.nanmean(normed_scores_eval,0)
+table_eval += '\\midrule \n'
+table_eval += 'Norm.score & ' + ' & '.join(normed_scores_eval.round(3).astype(str)) + r'\\ \bottomrule'
+table_eval += r"""
+\end{tabular}%
+}
+\label{tab:procgen}
+\end{table}%
+"""
+
+normed_scores_train = (reported_scores_train/np.tile(reported_scores_train[:,1].reshape(16,1),(1,len(CLEAN_NAMES_AGENTS) ) )) # standardized by PPO
+normed_scores_train[normed_scores_train == 0] = np.nan
+normed_scores_train = np.nanmean(normed_scores_train,0)
+table_train += '\\midrule \n'
+table_train += 'Norm.score & ' + ' & '.join(normed_scores_train.round(3).astype(str)) + r'\\ \bottomrule'
+table_train += r"""
+\end{tabular}%
+}
+\label{tab:procgen}
+\end{table}%
+"""
+print(table_eval)
+# print(table_train)
---
+++ coinrun/coinrun/wandb_scripts/download_wandb_data.py
@@ -0,0 +1,111 @@
+import pandas as pd
+import os
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+from collections import defaultdict
+os.environ["WANDB_API_KEY"] = "02e3820b69de1b1fcc645edcfc3dd5c5079839a1"
+os.environ["WANDB_API_KEY"] = "02e3820b69de1b1fcc645edcfc3dd5c5079839a1"
+import wandb
+import time
+import re
+
+def set_curve_attributes(agent,agent2label):
+        linewidth = 2
+        linestyle = '-'
+        do_not_plot = False
+        marker = None
+        label = agent
+        if label == 'ppo_goal' or label =='ppo_goal_bogdan':
+            col = 'red'
+        elif label == 'ppo':
+            col = 'limegreen'
+            linestyle = '--'
+        elif label == 'ppo_diayn':
+            col = 'fuchsia'
+            linestyle = '--'
+        elif label == 'ppg':
+            col = 'orange'
+            linestyle = '--'
+        elif label == 'ppo_curl':
+            col = 'coral'
+            linestyle = '-'
+        
+
+        label = agent2label[label]
+        
+        return label, col, linestyle, linewidth, marker, do_not_plot
+
+
+def load_WandB_csvs(files,params_to_load,selected_run_ids,AGENTS,agent2label):
+    df = []
+    for i,fh in enumerate(files): 
+        try:
+            run_df = pd.read_csv(fh,index_col='custom_step').drop(['_step','Unnamed: 0'],1)
+        except:
+            print('Skipping malformated CSV')
+            continue
+        run_id, agent = fh.split('/')[2].split('___')
+        agent = agent.split('.')[0]
+        group = '__'.join(run_id.split('__')[:-1])
+
+        if agent not in AGENTS:
+            continue
+        
+        if not re.findall(selected_run_ids[agent],group):
+            continue
+        label, col, linestyle, linewidth, marker, do_not_plot = set_curve_attributes(agent,agent2label)
+
+        run_df['agent'] = label
+        run_df['col'] = col
+        run_df['linestyle'] = linestyle
+        run_df['UID'] = np.random.randint(1000000)
+        run_df['group'] = group
+
+        with open(fh[:-4]+'.json','r') as json_fh:
+            params = json.load(json_fh)
+            for p in params_to_load:
+                if p not in params:
+                    params[p] = {}
+                    params[p]['value'] = 1
+                run_df[p] = params[p]['value']
+                
+        df.append(run_df)
+    df=pd.concat(df)
+    return df
+
+if __name__ == "__main__":
+
+    api = wandb.Api()
+
+    AGENTS = ["ppo_goal_bogdan","ppo","ppo_diayn","ppg",'ppo_curl']
+
+    METRICS = ['custom_step','eprew','eprew_eval','silhouette_score']
+
+    runs = api.runs("ssl_rl/procgen_generalization")
+    df = []
+    for i,run in enumerate(runs): 
+    
+        params = json.loads(run.json_config)
+        agent = params['agent']['value']
+        if agent not in AGENTS:
+            continue
+        
+        env_name = params['environment']['value']
+        RUN_METRICS = [env_name+'/'+metric for metric in METRICS]
+        run_df = run.history(keys=RUN_METRICS,samples=int(1e6))
+        if len(run_df) == 0:
+            run_df = run.history(keys=RUN_METRICS[:-1],samples=int(1e6))
+            
+        columns = run_df.columns
+        run_df.columns = [x.split('/')[-1] for x in columns]
+
+        filename = '%s___%s' % (run.name,agent)
+
+        run_df.to_csv(os.path.join('wandb_data',filename+'.csv'))
+
+        with open(os.path.join('..','wandb_data',filename+'.json'), 'w') as f:
+            json.dump(params, f)
+        print('Loaded ',i,run.name,'with %d rows'%len(run_df))
+        time.sleep(1)
