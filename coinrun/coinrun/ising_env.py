f8e4a8136fae1160dfeba8ff573cfc46aada6d95
ahmeda14960 <ahmedah@stanford.edu>
2021-05-08 17:47:15 +0000

    removed STD logging wandb

--- coinrun/coinrun/ising_env.py
+++ coinrun/coinrun/ising_env.py
@@ -11,11 +11,12 @@ class Ising():
     '''
 
         
-    def __init__(self,beta=1/4):
+    def __init__(self,beta=1/4,N=64):
         self.beta = beta
+        self.N = N
 
-    def reset(self,N):
-        config = 2*np.random.randint(2, size=(N,N))-1
+    def reset(self):
+        config = 2*np.random.randint(2, size=(self.N,self.N))-1
         self.config = config
         return config
         
@@ -60,38 +61,73 @@ class IsingEnv(gym.Env):
     def __init__(self,T=32,k=5):
         self.BETA_MIN = 0.01
         self.BETA_MAX = 0.3
-        self.H = 64
-        self.W = 64
+        self.N = 64
         self.MAX_T = T
 
+        self.k = k
+        self.T = T
+
         self.betas = np.linspace(self.BETA_MIN,self.BETA_MAX,k)
-        self.models = [Ising(beta=beta) for beta in self.betas] # make k different dynamics
+        self.models = [Ising(beta=beta,N=self.N) for beta in self.betas]
 
         self.action_space = gym.spaces.Discrete(k)
         self.observation_space = gym.spaces.Box(
             low=0,
             high=255,
-            shape=(1, self.H, self.W),
+            shape=(1, self.N, self.N),
             dtype=np.uint8,
         )
 
-        self.current_path = np.random.randint(0,k,1)
+        self.reset()
 
-        
+    def _rescale_state(self,state):
+        state = (state+1)/2 # between 0 and 1
+        noise = np.random.uniform(0,0.1,size=state.shape)
+        state = state + noise
+        state = ( state / (state.max()) * 255. ).astype(np.uint8)
+        return state
 
     def _generate_goal(self):
         goal_beta = np.random.uniform(self.BETA_MIN,self.BETA_MAX,1)
 
         self.goal_beta = goal_beta
 
-        self.goal_model = Ising(beta=goal_beta)
+        self.goal_model = Ising(beta=goal_beta,N=self.N)
+        self.goal_model.reset()
 
         T = np.random.randint(0,self.MAX_T)
 
         self.goal = self.goal_model.simulate_n(T).copy()
 
+    def reset(self):
+        self._generate_goal()
+        self.current_path = np.random.randint(0,self.k,1)
+        state = None
+        for i in range(len(self.models)):
+            if i == self.current_path:
+                state = self.models[i].reset()
+            else:
+                self.models[i].reset()
+        self.t = 0
+        return self._rescale_state(state)
 
-
+    def step(self,action):
+        self.current_path = action
+        state = None
+        for i in range(len(self.models)):
+            if i == self.current_path:
+                state = self.models[i].simulate_n(1).copy()
+            else:
+                self.models[i].simulate_n(1)
+           
+        reward = -np.log(np.linalg.norm(state-self.goal,ord=2)) # when ||s-goal||=0, reward=infty
+        self.t += 1
+        done = bool( self.t < self.T )
+        return self._rescale_state(state), reward, done, {}
 
 if __name__ == '__main__':
-    ising = IsingEnv(T=32,k=5)
+    env = IsingEnv(T=32,k=5)
+    state = env.reset()
+    a = env.action_space.sample()
+    s,r,done,_ = env.step(a)
+    print(s)
